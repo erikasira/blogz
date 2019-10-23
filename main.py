b6c2a0fe6640cdedb57b602bce0cfdd386b1fe64
Erika <erika.sira@gmail.com>
2019-10-23 15:25:04 -0400

    update two

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:1234@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = '1Lbsmih2'
+app.secret_key = 'y3568nwhbu8'
 
 
 class BlogPost(db.Model):                                       #This is creating a table that will store the blog info
@@ -34,10 +34,30 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+@app.route('/')
+def home():
+    users = User.query.all()
+    return render_template('home.html', users=users)
+
+
+@app.route('/blog', methods=['GET', 'POST'])                        #This is setting up the main page for the web application
+def index():
+
+    post_id = str(request.args.get('id'))  
+    mypost =  BlogPost.query.get(post_id)                                                 #This is setting a function so that the page works?
+    posts = BlogPost.query.all()                              #I think this line pulls the info from the SQL table?
+
+    return render_template('index.html', posts=posts, mypost=mypost)       #This will render the html template set for the index
+
+
+@app.route('/newpost', methods=['POST','GET'])                #This is another page, which is where the user will input their blog
+def newpost():
+    return render_template('newpost.html')
+
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup']
+    allowed_routes = ['login', 'signup', 'blog', 'index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
@@ -51,6 +71,7 @@ def login():
         if user and user.password == password:
             session['username'] = username
             flash("Logged in")
+            print(session)
             return redirect('/newpost')
         else:
             flash('User password incorrect or user does not exist', 'error')
@@ -69,34 +90,30 @@ def signup():
         verify_error = ""
 
         existing_user = User.query.filter_by(username=username).first()
-        new_user = User(username, password)
-        db.session.add(new_user)
-        db.session.commit()
-        session['username'] = username
+        if not existing_user:
+    #THIS IS FOR THE USERNAME ERROR
+            if not 20 >= len(username) >= 3 or " " in username:         # if the username is greater than 20 characters or less than three, or there is a space                
+                username_error = "Please enter a valid username. Username should be between 3-20 characters and cannot contain any spaces or periods."       # it will produce an error message
 
-#THIS IS FOR THE USERNAME ERROR
-        if not 20 >= len(username) >= 3 or " " in username:         # if the username is greater than 20 characters or less than three, or there is a space                
-            username_error = "Please enter a valid username. Username should be between 3-20 characters and cannot contain any spaces or periods."       # it will produce an error message
+        #THIS IS FOR THE PASSWORD ERROR
+            if not 20 >= len(password) >= 3 or " " in password:
+                password_error = "Please enter a valid password. Password should be between 3-20 characters and cannot contain any spaces or periods."       
 
-    #THIS IS FOR THE PASSWORD ERROR
-        if not 20 >= len(password) >= 3 or " " in password:
-            password_error = "Please enter a valid password. Password should be between 3-20 characters and cannot contain any spaces or periods."       
+        #THIS IS FOR VERIFYING THE PASSWORD
+            if verify != password:                                  
+                verify_error = "Password does not match."
 
-    #THIS IS FOR VERIFYING THE PASSWORD
-        if verify != password:                                  
-            verify_error = "Password does not match."
+            if not username_error and not password_error and not verify_error:
+                existing_user = User.query.filter_by(username=username).first()
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                                                                                                    
+                return redirect("/newpost?username={0}".format(username))                               
+            else:
 
-        if not username_error and not password_error and not verify_error:
-            existing_user = User.query.filter_by(username=username).first()
-            new_user = User(username, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['username'] = username
-                                                                                                
-            return redirect("/newpost?username={0}".format(username))                               
-        else:
-
-            return render_template('signup.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error)   #otherwise, give the user the form with the username in tact
+                return render_template('signup.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error)   #otherwise, give the user the form with the username in tact
     return render_template('signup.html')
 
 
@@ -104,22 +121,7 @@ def signup():
 @app.route('/logout', methods=['GET'])
 def logout():
     del session['username']
-    return redirect('/')
-
-
-@app.route('/blog', methods=['GET', 'POST'])                        #This is setting up the main page for the web application
-def index():
-
-    post_id = str(request.args.get('id'))  
-    mypost =  BlogPost.query.get(post_id)                                                 #This is setting a function so that the page works?
-    posts = BlogPost.query.all()                              #I think this line pulls the info from the SQL table?
-
-    return render_template('index.html', posts=posts, mypost=mypost)       #This will render the html template set for the index
-
-
-@app.route('/newpost', methods=['POST','GET'])                #This is another page, which is where the user will input their blog
-def newpost():
-    return render_template('newpost.html')
+    return redirect('/blog')
 
 
 @app.route('/add-post', methods=['POST', 'GET'])                      #I am trying to get this to actually post the blogposts to the page, but no idea...
@@ -134,10 +136,11 @@ def addpost():
         if not post:
             post_error = "please enter some text."
         if not title_error and not post_error:
-            newpost = BlogPost(title=title, post=post)
+            owner = User.query.filter_by(username=session['username']).first()
+            newpost = BlogPost(title, post, owner)
             db.session.add(newpost)
             db.session.commit()
-            return redirect('/blog?id={0}'.format(newpost.id))
+            return redirect('/blog?id=' + str(newpost.id))
         else:
             return render_template('newpost.html', title_error=title_error, post_error=post_error)
 
